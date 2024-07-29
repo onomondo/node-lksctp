331593aa3fdacb668cee90f3e9fc60ad5cebf8f2
Simon Kadisch <simon.kadisch@k13-engineering.at>
2024-07-29 08:19:12 +0200

    Added first simple test

---
+++ test/native.js
@@ -0,0 +1,14 @@
+const native = require("../lib/native.js");
+const assert = require("node:assert");
+
+describe("native", () => {
+    it("should create and close a socket correclty", () => {
+        const { errno: createErrno, fd } = native.create_socket();
+
+        assert(createErrno === 0);
+        assert(typeof fd === "number");
+
+        const { errno: closeErrno } = native.close_fd({ fd });
+        assert(closeErrno === 0);
+    });
+});
