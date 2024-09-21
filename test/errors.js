6d9243671498d6b7fd6f9fc4423abba40d6cfd7b
Simon Kadisch <simon.kadisch@k13-engineering.at>
2024-09-21 11:18:16 +0200

    Also test for duplex close event on error

--- test/errors.js
+++ test/errors.js
@@ -3,6 +3,25 @@ const socketpairFactory = require("./lib/socketpair.js");
 const { doesErrorRelateToCode } = require("./lib/error-util.js");
 const assert = require("node:assert");
 
+const waitForSocketError = ({ socket, timeoutMs = 500 }) => {
+  return new Promise((resolve, reject) => {
+
+    let errorReceived = undefined;
+
+    socket.on("error", (err) => {
+      errorReceived = err;
+    });
+
+    socket.on("close", () => {
+      resolve(errorReceived);
+    });
+
+    setTimeout(() => {
+      reject(new Error("timeout"));
+    }, timeoutMs);
+  });
+};
+
 describe("errors", () => {
   it("should give ECONNREFUSED when connecting to a non-listening port", async () => {
     const socket = lksctp.connect({
@@ -10,11 +29,11 @@ describe("errors", () => {
       host: "127.0.0.1",
     });
 
-    const error = await new Promise((resolve) => {
-      socket.on("error", (err) => {
-        resolve(err);
-      });
-    });
+    const error = await waitForSocketError({ socket });
+
+    if (error === undefined) {
+      throw Error("expected error");
+    }
 
     assert(doesErrorRelateToCode({ error, code: "ECONNREFUSED" }));
   });
