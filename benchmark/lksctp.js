08f216518ad0dc23110fb2278ef5434cfe61dc80
Peter S. Bornerup <pb@onomondo.com>
2025-09-02 09:44:51 +0200

    ...

--- benchmark/lksctp.js
+++ benchmark/lksctp.js
@@ -32,16 +32,14 @@ if (process.env.SERVER) {
 } else {
   benchmark.runClient({
     connect: () => {
-      return () => {
-        console.log("connecting to", process.env.SERVER_HOST);
-        return lksctp.connect({
-          host: process.env.SERVER_HOST,
-          port: 12345,
-          sctp: {
-            sack_freq: 1,
-          },
-        });
-      };
+      console.log("connecting to", process.env.SERVER_HOST);
+      return lksctp.connect({
+        host: process.env.SERVER_HOST,
+        port: 12345,
+        sctp: {
+          sack_freq: 1,
+        },
+      });
     },
   });
 }
